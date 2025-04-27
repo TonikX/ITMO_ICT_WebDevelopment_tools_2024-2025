153a360b4bfe0aabf69de663164405ca444af0af
Ekaterina Buyakova <ebuakova684@gmail.com>
2025-04-27 14:18:17 +0300

    async sum updated

--- Students/k3344/Ekaterina_Buyakova/Lr2/task1/async_sum.py
+++ Students/k3344/Ekaterina_Buyakova/Lr2/task1/async_sum.py
@@ -1,40 +1,51 @@
 import asyncio
 import time
-from concurrent.futures import ThreadPoolExecutor
 
-def calculate_sum_of_range(start, end):
+async def calculate_sum_of_range(start, end):
     total = 0
     for number in range(start, end):
         total += number
+        if number % 10000000 == 0:
+            await asyncio.sleep(0)
     return total
 
 async def calculate_sum(limit, num_tasks):
     print(f"Вычисляем сумму от 1 до {limit} с помощью {num_tasks} async-задач...")
 
     start_time = time.time()
-    chunk_size = limit // num_tasks
 
+    chunk_size = limit // num_tasks
     ranges = [
         (i * chunk_size + 1, (i + 1) * chunk_size + 1 if i != num_tasks - 1 else limit + 1)
         for i in range(num_tasks)
     ]
 
-    loop = asyncio.get_running_loop()
-    with ThreadPoolExecutor(max_workers=num_tasks) as executor:
-        tasks = [
-            loop.run_in_executor(executor, calculate_sum_of_range, start, end)
-            for start, end in ranges
-        ]
-        results = await asyncio.gather(*tasks)
+    tasks = [
+        asyncio.create_task(calculate_sum_of_range(start, end))
+        for start, end in ranges
+    ]
+
+    results = await asyncio.gather(*tasks)
 
     total_sum = sum(results)
     end_time = time.time()
 
+    execution_time = end_time - start_time
+
     print(f"Сумма: {total_sum}")
     print(f"Async-задачи: {num_tasks}")
-    print(f"Время выполнения: {end_time - start_time:.3f} секунд")
+    print(f"Время выполнения: {execution_time:.3f} секунд")
+
+    return num_tasks, execution_time
 
 if __name__ == "__main__":
     limit = 1000000000
-    for threads in range(4, 5):
-        asyncio.run(calculate_sum(limit, threads))
+    results = []
+
+    for tasks in range(4, 5):
+        num_tasks, execution_time = asyncio.run(calculate_sum(limit, tasks))
+        results.append((num_tasks, execution_time))
+
+    print("Результаты:")
+    for num_tasks, execution_time in results:
+        print(f"{num_tasks} задач — {execution_time:.3f} секунд")
