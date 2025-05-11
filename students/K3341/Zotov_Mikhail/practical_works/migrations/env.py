7c3674012129117513c44405dd93460eaaaceede
Waycoolers <mihazd01@mail.ru>
2025-05-11 17:14:57 +0300

    moving practical work

--- students/K3341/Zotov_Mikhail/practical_works/migrations/env.py
+++ students/K3341/Zotov_Mikhail/practical_works/migrations/env.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url = os.getenv("DB_ADMIN")
+db_url = os.getenv("WARRIORS_DB")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
