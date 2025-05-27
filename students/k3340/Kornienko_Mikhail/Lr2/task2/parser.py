d0fa31713721ae9cb95cbeb497174653117c273a
mishakorni <mikekornienko04@gmail.com>
2025-05-27 23:34:48 +0300

    lr3 is done

--- students/k3340/Kornienko_Mikhail/Lr2/task2/parser.py
+++ students/k3340/Kornienko_Mikhail/Lr2/task2/parser.py
@@ -1,25 +1,31 @@
+import os
+
 import requests
+from alembic.templates.multidb.env import db_names
 from bs4 import BeautifulSoup
 from sqlmodel import SQLModel, create_engine, Session, select
 from models import *
+import random
 
-db_url = 'postgresql://dbuser:12345678@localhost:25432/db'
+db_url = os.getenv('DB_URL', 'postgresql://dbuser:12345678@localhost:25432/db')
 engine = create_engine(db_url, echo=True)
 SQLModel.metadata.create_all(engine)
 
+# def parse_task(url):
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#
+#     title = soup.find('h2', class_='problem_title')
+#
+#     text = ''
+#     texts = soup.find_all('div', class_='problem_par_normal')
+#     for t in texts:
+#         text += t.text.strip() + '\n'
+#
+#     return title.text, text
 
 def parse_task(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    title = soup.find('h2', class_='problem_title')
-
-    text = ''
-    texts = soup.find_all('div', class_='problem_par_normal')
-    for t in texts:
-        text += t.text.strip() + '\n'
-
-    return title.text, text
+    return f"Mock title {random.randint(1, 1000)}", f"Mock text {random.randint(1, 1000)}"
 
 def save_task(title, text):
     with Session(engine) as session:
