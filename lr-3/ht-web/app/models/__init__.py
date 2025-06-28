4a1f92611b2d08600c00ddcb79345d3fd063be50
VladislavRkm <sl3epy.1rkm1@yandex.ru>
2025-06-28 04:20:44 +0300

    Revert "lr-3 ok"
    This reverts commit c9a5bb7b6fe9a016726f5b24a6f7d34b4cc1a7fa.

--- lr-3/ht-web/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .book import Book
-from .user_book import UserBook
-from .exchange_request import ExchangeRequest
-from .exchange_status_history import ExchangeStatusHistory 
