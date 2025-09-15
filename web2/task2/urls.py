d06ff8a06ee64b55818665bb28098bd9455e55b9
tiltek1d <113026367+tiltek1d@users.noreply.github.com>
2025-09-15 21:23:57 +0300

    done lab

---
+++ web2/task2/urls.py
@@ -0,0 +1,9 @@
+urls =[
+    "https://www.хакатоны.рус/tpost/9hidyk5dx1-technoforge-agrotech",
+    "https://www.хакатоны.рус/tpost/jvv3dc8kg1-mezhregionalnii-konkurs-po-igrovoi-razra",
+    "https://www.хакатоны.рус/tpost/5fbnaj8g51-anomaly-zone-game-jam",
+    "https://www.хакатоны.рус/tpost/eculsok1x1-unithack-2025",
+    'https://www.хакатоны.рус/tpost/imklbzv241-belie-hakeri',
+    'https://www.хакатоны.рус/tpost/l8jyol49s1-rlttenderguide-intellektualnaya-sistema',
+    'https://www.хакатоны.рус/tpost/r5b0v909h1-lideri-tsifrovoi-transformatsii-2025',
+]
